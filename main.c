f5f9e9596e530a137f4cf08bd5ca94dbc7a46253
augustocolo <33198795+augustocolo@users.noreply.github.com>
2017-11-14 16:47:01 +0100

    worked on importTrafficDB and importPlaneDB added calcMonoTime

--- main.c
+++ main.c
@@ -30,13 +30,16 @@ nodesInfo node[MAXLENGTHOFARRAY];
 int importAirportFile(char);
 
 //Planes Database Variables&Types&Functions
+int npldb=0;
 typedef struct pDb{
     char model [4 + 1];
     char type [2 + 1];
 }planeDB;
 planeDB plane[MAXLENGTHOFARRAY];
+int importPlaneDatabase ();
 
 //Traffic Database Variables&Types&Functions
+int ntra=0;
 typedef struct tDb{
     int first_hh;
     int first_mm;
@@ -56,8 +59,14 @@ typedef struct tDb{
 trafficDatabase traffic[MAXLENGTHOFARRAY];
 int importTrafficDatabase (char);
 
+int calcMonoTime(int, int, int);
+
 int main() {
     char filename[16];
+    if (importPlaneDatabase()!=0){
+        printf("ERROR WHILE IMPORTING PLANES DATABASE");
+        return 1;
+    }
     printf("AIRPORT MANAGER v2.0 by Augusto Colongo\nType the airport that you'd like to use: ");
     gets(filename);
     strcat(filename,".dat");
@@ -72,6 +81,7 @@ int main() {
         printf("ERROR WHILE IMPORTING TRAFFIC DATABASE");
         return 1;
     }
+    system("cls"); //is this working? IDK
 
 
 
@@ -83,7 +93,7 @@ int importAirportFile(char name){
     int i=0;
     // test
     FILE *fp_airportinfo;
-    if((fp_airportinfo=fopen(name, "r"))==NULL) {
+    if((fp_airportinfo=fopen((const char *) name, "r")) == NULL) {
         return 1;
     }
     fscanf(fp_airportinfo, "%d\t\t\t\t\t\t\n", &nair);
@@ -115,6 +125,67 @@ int importAirportFile(char name){
 }
 
 int importTrafficDatabase(char name){
+    int i=0, j=0;
+    int planefound=false;
+    FILE *fp_trafficinfo;
+    if((fp_trafficinfo=fopen((const char *) name, "r")) == NULL){
+        return 1;
+    }
+    fscanf(fp_trafficinfo, "%d\t\t\t\t\n", &ntra);
+    for (i=0; i<ntra,i++){
+        fscanf(fp_trafficinfo,"%d:%d:%d\t%c\t%s\t%s\t%s\t\n",
+               &traffic[i].first_hh,
+               &traffic[i].first_mm,
+               &traffic[i].first_ss,
+               &traffic[i].event_action,
+               traffic[i].plane_regNum,
+               traffic[i].plane_model,
+               traffic[i].airport);
+        traffic[i].plane_alreadyBeen=0;
+        for (j=0;j<i;j++){
+            if (strcmp(traffic[i].plane_regNum,traffic[j].plane_regNum)==0) {
+                traffic[i].plane_alreadyBeen++;
+            }
+        }
+        for (j=0;j<npldb && planefound==false,j++){
+            if (strcmp(traffic[i].plane_model,plane[j].model)==0){
+                strcpy(traffic[i].plane_type,plane[j].type);
+                planefound=true;
+            } else if (j==npldb-1){
+                printf("error while comparing traffic database with plane database: model %s not found\n", traffic[i].plane_model);
+                return 1;
+            }
+        }
+        //event time = first time
+        traffic[i].event_hh= traffic[i].first_hh;
+        traffic[i].event_mm= traffic[i].first_mm;
+        traffic[i].event_ss= traffic[i].first_ss;
+        traffic[i].first_time= calcMonoTime(traffic[i].first_hh,
+                                            traffic[i].first_mm,
+                                            traffic[i].first_ss);
+        traffic[i].event_time= traffic[i].first_time;
+    }
+    fclose(fp_trafficinfo);
+    return 0;
+}
+
+int importPlaneDatabase (){
     int i=0;
-    
+    FILE *fp_planedatabase;
+    if((fp_planedatabase=fopen("PlaneDB.dat","r"))==NULL){
+        return 1
+    }
+    fscanf(fp_planedatabase, "%d\t\n", &npldb);
+    for (i=0;i<npldb;i++){
+        fscanf(fp_planedatabase,"%s\t%s\n",
+        plane[i].model,
+        plane[i].type);
+    }
+    fclose(fp_planedatabase);
+}
+
+int calcMonoTime(int hh, int mm, int ss){
+    int monotime;
+    monotime= (hh*100000)+(mm*1000)+ss;
+    return monotime;
 }
